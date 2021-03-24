a14b2e80d4f60303c44cd0620ade7eccb3857419
Guillermo Ramos <gramos@gramos.me>
2021-03-24 12:11:24 +0100

    Add test for custom mappers

--- test/bro_test.exs
+++ test/bro_test.exs
@@ -2,6 +2,10 @@ defmodule BroTest do
   use ExUnit.Case
   doctest Bro
 
+  defmodule MyPoint do
+    defstruct [x: nil, y: nil, z: nil]
+  end
+
   defmodule TestMod do
     import Bro
 
@@ -12,11 +16,15 @@ defmodule BroTest do
 
       # This header defines a 'mysterious' record, which defines a tricky 'fn' field
       defheader("test/keywords.hrl", records: [:normal, :mysterious])
+
+      # Custom converters
+      def from_record({:point, x, y, z}), do: %MyPoint{x: x, y: y, z: z}
+      def to_record(%MyPoint{x: x, y: y, z: z}), do: {:point, x, y, z}
     end
 
   end
 
-  alias TestMod.{Message, Account}
+  alias TestMod.{Message, Account, Point}
 
   test "Check only exported records are defined" do
     assert Keyword.has_key?(Message.__info__(:macros), :message) == true
@@ -44,5 +52,12 @@ defmodule BroTest do
       Account.account(name: "gramos", host: :undefined)
     assert %Account{name: "gramos", host: nil} ==
       Account.account(name: "gramos", host: :undefined) |> TestMod.from_record()
+  end
+
+  test "Custom mappers" do
+    require TestMod.Point
+
+    assert %MyPoint{x: 1, y: 2, z: 3} ==
+      Point.point(x: 1, y: 2, z: 3) |> TestMod.from_record()
   end
 end
